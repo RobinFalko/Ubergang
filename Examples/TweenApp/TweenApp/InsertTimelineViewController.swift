9125123cb686c3d5e7b409445be427848cbf074b
Robin Frielingsdorf <Fuzz@RFs-MacBook-Pro.fritz.box>
2017-08-13 12:32:54 +0200

    Change insert(tween:at:) example

--- Examples/TweenApp/TweenApp/InsertTimelineViewController.swift
+++ Examples/TweenApp/TweenApp/InsertTimelineViewController.swift
@@ -24,16 +24,18 @@ class InsertTimelineViewController: ExampleViewController {
                 self.tweenControls.stop()
             }
         
+        //time 0 to 0.5
         timeline.append(CGFloat(0).tween(to: 100).update({ [unowned self] (value: CGFloat) in
                 self.viewCenterY.constant = value
             })
-            .duration(2)
         )
         
+        //time 4 to 4.5
         timeline.insert(CGFloat(0).tween(to: 100).update({ [unowned self] in
             self.viewCenterX.constant = $0
         }), at: 4)
         
+        //time 5 to 7
         timeline.insert(CGFloat(100).tween(to: -100).update({ [unowned self] (value: CGFloat) in
                 self.viewCenterX.constant = value
                 self.viewCenterY.constant = value
@@ -41,15 +43,17 @@ class InsertTimelineViewController: ExampleViewController {
             .duration(2)
         , at: 5)
         
+        //time 7 to 9
         timeline.insert(CGFloat(-100).tween(to: 0).update({ [unowned self] (value: CGFloat) in
                 self.viewCenterX.constant = value
             })
             .duration(2)
         , at: 7)
         
+        //time 9.5 to 10
         timeline.insert(CGFloat(-100).tween(to: 0).update({ [unowned self] in
             self.viewCenterY.constant = $0
-        }), at: 10)
+        }), at: 9.5)
         
         return timeline
     }
